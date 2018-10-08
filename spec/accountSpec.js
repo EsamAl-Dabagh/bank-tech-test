8b3eccdde3a5c1e148087f456d6a197ae9992783
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-08 11:13:01 +0100

    feat: create addFunds() - it increases the balance by amount

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -8,4 +8,12 @@ describe("Account", function () {
   it("initializes with a balance of zero", function () {
     expect(account._balance).toEqual(0);
   });
+
+  describe("#addFunds", function() {
+    it("increases the balance", function() {
+      account.addFunds(100);
+
+      expect(account._balance).toEqual(100);
+    });
+  });
 });
--- src/account.js
+++ src/account.js
@@ -4,5 +4,9 @@
     this._balance = 0;
   }
 
+  Account.prototype.addFunds = function(amount) {
+    this._balance += amount;
+  }
+
   exports.Account = Account;
 })(this)
