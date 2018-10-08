22d4aefffbee347e6dfe8b99023cc547c84efe0b
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-08 14:25:58 +0100

    feat: Statement#update pushes current transaction info  into _transaction array

--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -1,13 +1,41 @@
-describe("Statement", function() {
+describe("Statement", function () {
 
   var statement;
 
-  beforeEach(function() {
+  beforeEach(function () {
     statement = new Statement();
   });
-  
-  it("initializes with empty transactions array", function() {
+
+  it("initializes with empty transactions array", function () {
     expect(statement._transactions).toEqual([]);
+  });
+
+  describe("#update", function () {
+    describe("pushes transaction to _transactions array", function(){
+
+      beforeEach(function() {
+        statement.update("+", 500, 1000);
+      });
+
+      it("increases transactions array by 1", function () {
+        expect(statement._transactions.length).toEqual(1);
+      });
+
+      it("adds amount of transaction", function() {
+        expect(statement._transactions[0].amount).toEqual(500);
+      });
+
+      it("adds type of transaction", function() {
+        expect(statement._transactions[0].type).toEqual("+");
+      });
+      
+      it("adds balance of transaction", function() {
+        expect(statement._transactions[0].balance).toEqual(1000);
+      });
+    });
+    
+    
+    
   });
 
 })
--- src/statement.js
+++ src/statement.js
@@ -4,6 +4,16 @@
     this._transactions = [];
   }
 
+  Statement.prototype.update = function(type, amount, balance) {
+    var dateObj = new Date();
+    var dateString = `${dateObj.getDate()}/${dateObj.getMonth()}/${dateObj.getFullYear()}}` 
+    this._transactions.push({
+      date: dateString,
+      type: type,
+      amount: amount,
+      balance: balance
+    });
+  }
 
   exports.Statement = Statement;
 })(this)
